ea8921600667649afebfbfcc6bb2dde8f83eae23
Ali <ali.vexll0@gmail.com>
2024-05-30 20:29:27 +0300

    last version

--- DB/queries/drawning_db.py
+++ DB/queries/drawning_db.py
@@ -25,8 +25,24 @@ class DrawningDB:
             cursor.execute(query, (city, latitude, longitude, datetime))
             conn.commit()
             print("the case has inserted successfully")
+            conn.close()
         except sqlite3.Error as e:
             print("SQLite error:", e)
-        finally:
+
+
+
+    @staticmethod
+    def view_all_cases():
+        try:
+            conn = Database.connection()
+            conn.row_factory = sqlite3.Row
+            query = "SELECT * FROM drowning"
+            cursor = conn.cursor()
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            result = {row['id']: dict(row) for row in rows}
             conn.close()
+            return result
+        except sqlite3.Error as e:
+            print("SQLite error:", e)
 
